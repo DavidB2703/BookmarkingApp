f3f1a853a078163eabea333d104cd249b3bd7382
mariodeaconescu <mariodeaconescu2003@gmail.com>
2023-12-30 13:51:07 +0200

    BookmarkCategories

---
+++ SocialBookmarkingApp/Migrations/20231219123845_BookmarkCategories.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialBookmarkingApp.Migrations
+{
+    public partial class BookmarkCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookmarks_Categories_CategoryId",
+                table: "Bookmarks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookmarks_CategoryId",
+                table: "Bookmarks");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Bookmarks");
+
+            migrationBuilder.CreateTable(
+                name: "BookmarkCategory",
+                columns: table => new
+                {
+                    BookmarksId = table.Column<int>(type: "int", nullable: false),
+                    CategoriesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookmarkCategory", x => new { x.BookmarksId, x.CategoriesId });
+                    table.ForeignKey(
+                        name: "FK_BookmarkCategory_Bookmarks_BookmarksId",
+                        column: x => x.BookmarksId,
+                        principalTable: "Bookmarks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookmarkCategory_Categories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookmarkCategory_CategoriesId",
+                table: "BookmarkCategory",
+                column: "CategoriesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookmarkCategory");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Bookmarks",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookmarks_CategoryId",
+                table: "Bookmarks",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookmarks_Categories_CategoryId",
+                table: "Bookmarks",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ SocialBookmarkingApp/Migrations/20231219125133_BookmarkCategory2.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialBookmarkingApp.Migrations
+{
+    public partial class BookmarkCategory2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserBookmark");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserBookmark",
+                columns: table => new
+                {
+                    SavedBookmarksId = table.Column<int>(type: "int", nullable: false),
+                    SavedById = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserBookmark", x => new { x.SavedBookmarksId, x.SavedById });
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserBookmark_AspNetUsers_SavedById",
+                        column: x => x.SavedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserBookmark_Bookmarks_SavedBookmarksId",
+                        column: x => x.SavedBookmarksId,
+                        principalTable: "Bookmarks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserBookmark_SavedById",
+                table: "ApplicationUserBookmark",
+                column: "SavedById");
+        }
+    }
+}
